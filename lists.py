baccc3fafc31b2b0a9c6db854bc4581a0740f682
Gastón Gadea <53276272+gastongadea@users.noreply.github.com>
2025-04-30 16:04:46 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,25 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    return list_to_remove_elements[1:4] + list_to_remove_elements[6:]
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    #list_to_add_elements=['Pink']+list_to_add_elements+['Yellow']
+    list_to_add_elements.insert(0,'Pink')
+    list_to_add_elements.append('Yellow')
+    return list_to_add_elements
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    return list_to_check == []
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    return list_to_compare1[2:3]==list_to_compare2[2:3]
 
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    l2=[]
+    l2.append(list_of_lists_to_modify[0][:3])
+    l2.append(list_of_lists_to_modify[1:4])
+    l2.append(list_of_lists_to_modify[-2:])
+    return l2
